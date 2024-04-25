218eb3cd11d4627e8ac4d507213ac959233bc9ff
Gergely Hegybiro <hegybirog@gmail.com>
2024-04-25 16:10:42 +0200

    e1

--- src/Pinto/BlueScreen/assets/bluescreen.js
+++ src/Pinto/BlueScreen/assets/bluescreen.js
@@ -1,5 +1,5 @@
 /**
- * This file is part of the Zoxigen Framework
+ * This file is part of the Pinto Framework
  */
 
 class BlueScreen
@@ -9,30 +9,30 @@ class BlueScreen
 
 		let blueScreen = document.getElementById('Pinto-bs');
 
-		document.documentElement.classList.add('Zoxigen-bs-visible');
+		document.documentElement.classList.add('Pinto-bs-visible');
 		if (navigator.platform.indexOf('Mac') > -1) {
-			blueScreen.classList.add('Zoxigen-mac');
+			blueScreen.classList.add('Pinto-mac');
 		}
 
-		blueScreen.addEventListener('Zoxigen-toggle', (e) => {
-			if (e.target.matches('#Zoxigen-bs-toggle')) { // blue screen toggle
-				document.documentElement.classList.toggle('Zoxigen-bs-visible', !e.detail.collapsed);
+		blueScreen.addEventListener('Pinto-toggle', (e) => {
+			if (e.target.matches('#Pinto-bs-toggle')) { // blue screen toggle
+				document.documentElement.classList.toggle('Pinto-bs-visible', !e.detail.collapsed);
 
-			} else if (!e.target.matches('.Zoxigen-dump *') && e.detail.originalEvent) { // panel toggle
-				e.detail.relatedTarget.classList.toggle('Zoxigen-panel-fadein', !e.detail.collapsed);
+			} else if (!e.target.matches('.Pinto-dump *') && e.detail.originalEvent) { // panel toggle
+				e.detail.relatedTarget.classList.toggle('Pinto-panel-fadein', !e.detail.collapsed);
 			}
 		});
 
 		if (!ajax) {
 			document.body.appendChild(blueScreen);
-			let id = location.href + document.querySelector('.Zoxigen-section--error').textContent;
-			Pinto.Toggle.persist(blueScreen, sessionStorage.getItem('Zoxigen-toggles-bskey') === id);
-			sessionStorage.setItem('Zoxigen-toggles-bskey', id);
+			let id = location.href + document.querySelector('.Pinto-section--error').textContent;
+			Pinto.Toggle.persist(blueScreen, sessionStorage.getItem('Pinto-toggles-bskey') === id);
+			sessionStorage.setItem('Pinto-toggles-bskey', id);
 		}
 
 		(new ResizeObserver(stickyFooter)).observe(blueScreen);
 
-		if (document.documentElement.classList.contains('Zoxigen-bs-visible')) {
+		if (document.documentElement.classList.contains('Pinto-bs-visible')) {
 			window.scrollTo(0, 0);
 		}
 	}
@@ -68,9 +68,9 @@ class BlueScreen
 }
 
 function stickyFooter() {
-	let footer = document.querySelector('#Zoxigen-bs footer');
-	footer.classList.toggle('Zoxigen-footer--sticky', false); // to measure footer.offsetTop
-	footer.classList.toggle('Zoxigen-footer--sticky', footer.offsetHeight + footer.offsetTop - window.innerHeight - document.documentElement.scrollTop < 0);
+	let footer = document.querySelector('#Pinto-bs footer');
+	footer.classList.toggle('Pinto-footer--sticky', false); // to measure footer.offsetTop
+	footer.classList.toggle('Pinto-footer--sticky', footer.offsetHeight + footer.offsetTop - window.innerHeight - document.documentElement.scrollTop < 0);
 }
 
 let Pinto = window.Pinto = window.Pinto || {};
